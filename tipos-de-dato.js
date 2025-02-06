48a15269e7cab68242fc7f802752f2d39aa84907
AilynMza <ailynmorales501@gmail.com>
2025-02-05 20:46:44 -0600

    feat: Creación de tipos de datos. Index, titulo e imagen.

---
+++ tipos-de-dato.js
@@ -0,0 +1,8 @@
+// Tipos de datos
+console.log(typeof 46);
+console.log(typeof "Hola mundo");
+console.log(typeof true);
+var variableDeEjemplo = null;
+console.log(variableDeEjemplo);
+var variableDeEjemplo2;
+console.log(typeof variableDeEjemplo2);
