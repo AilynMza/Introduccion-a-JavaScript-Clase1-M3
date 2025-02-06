cb71488a572ca188487af14a04a3a0f797cdb34d
AilynMza <ailynmorales501@gmail.com>
2025-02-05 20:56:00 -0600

    feat: resolución de ejemplos y creación de más ejemplos.

--- tipos-de-dato.js
+++ tipos-de-dato.js
@@ -6,3 +6,25 @@ var variableDeEjemplo = null;
 console.log(variableDeEjemplo);
 var variableDeEjemplo2;
 console.log(typeof variableDeEjemplo2);
+
+// Ejemplos
+console.log(typeof 42); //number
+console.log(typeof "Veinticinco"); //string
+console.log(typeof -666); //number
+console.log(typeof true); //boolean
+console.log(typeof 0); //number
+console.log(typeof ""); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof FALSE); //undefined
+
+// Más ejemplos
+console.log(typeof 3.14); //number
+console.log(typeof NaN); //number
+console.log(typeof {}); //object
+var masEjemplos = null;
+console.log(masEjemplos); //null
+var masEjemplos2;
+console.log(typeof masEjemplos2); //undefined
+console.log(typeof BigInt(9007199254740991)); //bigint
+console.log(typeof Symbol("*")); //symbol
