bf30149835391e6c98c0d5b7383e4c186a351e75
AilynMza <ailynmorales501@gmail.com>
2025-02-05 20:57:49 -0600

    feat: se agrega los documentos de ejemplos vistos en clase.

---
+++ main-clase.js
@@ -0,0 +1,27 @@
+// Este es mi primer script en JavaScript
+
+console.log(5);
+console.log(typeof 5);
+
+console.log("Hola mundo");
+console.log(typeof "Hola mundo");
+
+/* Tipos de datos en JavaScript 
+Es importante conocerlos */
+
+console.log(true);
+console.log(typeof true);
+
+console.log(5 + "5");
+
+let nombre = "Diego";
+
+console.log("Hola " + nombre + "¿Cómo estas?");
+
+let numeroEspecial = 5;
+let numeroEspecialTexto = "5";
+
+console.log(numeroEspecial + parseInt(numeroEspecialTexto));
+console.log(parseFloat(numeroEspecial) + numeroEspecialTexto);
+
+let textoAleatorio = "Hola mundo";
